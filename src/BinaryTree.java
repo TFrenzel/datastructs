8c6ff6a2b8ef1a029441301d114b10f3db7c07f4
Travis <goose945@gmail.com>
2013-01-16 15:53:50 -0800

    Implemented Tests

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -13,6 +13,44 @@ public class BinaryTree {
 		}
 	}
 	
+	public BinaryTree(){
+		root = null;
+	}
+	
+	public void insert(int data){
+		root = insert(root, data);
+	}
+	
+	public boolean lookup(int data){
+		return (lookup(root, data));
+	}
+	
+	
+	private boolean lookup(Node node, int data){
+		if(node == null){
+			return false;
+		}
+		
+		if (node.data == data){
+			return true;
+		}else if (node.data < data){
+			return lookup(node.left, data);
+		}else{
+			return lookup(node.right,data);
+		}
+	
+	}
+	
+	private Node insert(Node node, int data){
+		if(node==null){
+			node = new Node(data);
+		}else if(data < node.data){
+			node.left = insert(node.left, data);
+		}else{
+			node.right = insert(node.right, data);
+		}
+		return (node);
+	}
 	
 	
 	
---
+++ src/BinaryTreeTest.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import java.util.Random;
+
+/**
+ * @author TFrenzel
+ *
+ */
+public class BinaryTreeTest {
+
+	/**
+	 * Test method for {@link BinaryTree#BinaryTree()}.
+	 */
+	@Test
+	public void testBinaryTree() {
+		System.out.println("Creating Tree");
+		BinaryTree tree = new BinaryTree();
+		assert (tree) != null;
+		System.out.println("Passed!");
+	}
+
+	/**
+	 * Test method for {@link BinaryTree#insert(int)}.
+	 */
+	@Test
+	public void testInsert() {
+		BinaryTree tree = new BinaryTree();
+		System.out.println("Insert 10 then lookup");
+		tree.insert(10);
+		assert(tree.lookup(10)==true);
+		System.out.println("Passed!");
+		System.out.println("Insert 5, 13, 15, 14 then lookup each");
+		tree.insert(5);
+		tree.insert(13);
+		tree.insert(15);
+		tree.insert(14);
+		assert(tree.lookup(5)==true);
+		assert(tree.lookup(13)==true);
+		assert(tree.lookup(15)==true);
+		assert(tree.lookup(14)==true);
+		System.out.println("Passed!");
+	}
+	
+	/**
+	 * Test method for {@link BinaryTree#lookup()}.
+	 */
+	@Test
+	public void testLookup() {
+		BinaryTree tree = new BinaryTree();
+		assert (tree) != null;
+		Random ran = new Random();
+		for(int i = 0; i < 25; i++){
+			tree.insert(ran.nextInt(100));
+		}
+		tree.insert(42);
+		assert(tree.lookup(42));
+		System.out.println("Passed!");
+	}
+
+	
+	
+}
