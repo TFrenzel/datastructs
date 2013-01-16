8b64fe9ab941a0cc74023deede061e904826222e
Travis <goose945@gmail.com>
2013-01-16 14:07:06 -0800

    Initial Commit

---
+++ src/BinaryTree.java
@@ -0,0 +1,19 @@
+public class BinaryTree {
+	private Node root;
+	
+	private class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		Node(int newData){
+			data = newData;
+			left = null;
+			right = null;
+		}
+	}
+	
+	
+	
+	
+}
